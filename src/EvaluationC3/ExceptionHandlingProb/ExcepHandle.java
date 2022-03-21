1d0432238d5664471af7db1d62c9dbe9b0fba35c
0Raj <rajasekark0@gmail.com>
2022-03-21 10:54:59 +0530

    Evaluation c3

---
+++ src/EvaluationC3/ExceptionHandlingProb/ExcepHandle.java
@@ -0,0 +1,13 @@
+package EvaluationC3.ExceptionHandlingProb;
+
+public class ExcepHandle {
+    //What is the difference between the throw and throws keyword in Java?
+    /* Throw:
+        1] This keyword is used to return an Exception manually when an exception occurs.
+        2] We can throw both checked and unchecked exception
+    * /
+        Throws:
+        1] This keyword is used in method signature to alert user that this method will throw an exception
+        2] using Throws keyword we can handle exception according to the exception type.
+     */
+}
