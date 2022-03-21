3f2c86277dcdf7e4d2fcffb683f31e994450eb88
0Raj <rajasekark0@gmail.com>
2022-03-21 10:59:03 +0530

    Evaluation c3

--- src/EvaluationC3/ExceptionHandlingProb/ExcepHandle.java
+++ src/EvaluationC3/ExceptionHandlingProb/ExcepHandle.java
@@ -10,4 +10,18 @@ public class ExcepHandle {
         1] This keyword is used in method signature to alert user that this method will throw an exception
         2] using Throws keyword we can handle exception according to the exception type.
      */
+
+    //What is the difference between Checked and Unchecked Exceptions in Java,
+    // create a user-defined checked and unchecked exception class?
+     /* Checked:
+        Checked exeption are also called as compli time exception
+        Checked exeption are identified during compile time itself.
+        we can't create .java file when checked exception occurs.
+
+        UnChecked;
+        UnChecked exeption are also called as runtime exception
+        UnChecked exeption are identified only during runtime.
+        we can create .java file when UnChecked exception occurs.
+        but we cannot run the program
+      */
 }
