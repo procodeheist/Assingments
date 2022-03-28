914ebec068e70bf54e836a774415cc48811427f1
0Raj <rajasekark0@gmail.com>
2022-03-28 10:23:58 +0530

    Evaluation c4 DeadLock

---
+++ src/EvaluationC4/DeadLock/DeadLock.java
@@ -0,0 +1,12 @@
+package EvaluationC4.DeadLock;
+
+public class DeadLock {
+    /*Explain the meaning of the deadlock and when it can occur in multithreading applications.*/
+    /*
+    * Deadlock is one kind of problem we face in Multithreaded application. In java our application will be keep on running without completing or throwing exception.
+    * The main reason for deadlock is synchronized method or class which are dependent on each other.
+    * When one synchronized method or class is dependent on other synchronized method or class we face deadlock scenario.
+    * We can prevent deadlock by providing one synchronized method or class if two or three methods dependent on each other.
+    * */
+
+}
